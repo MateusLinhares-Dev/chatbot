b42b65e8626025a08e1697af35dc0bbd060aabab
Mateus Epifanio Linhares <mlinhares@softexpert.com>
2025-09-24 16:08:11 -0300

    alteração

--- src/services/bot/bot.ts
+++ src/services/bot/bot.ts
@@ -1,9 +1,8 @@
-import { ActivityHandler, ConversationState, TurnContext, UserState, StatePropertyAccessor } from 'botbuilder';
+import { ActivityHandler, ConversationState, TurnContext, UserState, StatePropertyAccessor, InvokeResponse } from 'botbuilder';
 import { userStateInfo } from '../../dto/interfaceInfoUserState';
 import { DispatcherHandlerUser } from '../user/handlers/dispatcherUser';
 import { DispatchController } from '../user/dispatch';
 import { UserDataError } from "../../domain/error/UserError";
-
 
 export class EchoBot extends ActivityHandler {
     private UserState: UserState;
@@ -17,10 +16,11 @@ export class EchoBot extends ActivityHandler {
         this.ConversationState = ConversationState;
         this.userProfileAccessor = this.UserState.createProperty<userStateInfo>('UserProfile');
         
-        this.dispatcher = new DispatchController()
+        this.dispatcher = new DispatchController();
         this.dispatcher.registerHandler('user', new DispatcherHandlerUser(this.userProfileAccessor));
 
         this.onMessage(async (context, next) => {
+            console.log(context.activity)
             try {
                 const profile = await this.dispatcher.getHandler(context, 'user');
                 await context.sendActivity(`Olá ${profile.name}`);
@@ -28,34 +28,27 @@ export class EchoBot extends ActivityHandler {
                 throw new UserDataError('Error invalid user!', err as Error);
             }
 
-
             await next();
         });
-        
-    }
 
-    protected async onInvokeActivity(context: TurnContext): Promise<any> {
-        console.log('entrei no on')
-        if (context.activity.name === 'adaptiveCard/action') {
-            const action = context.activity.value;
-            console.log("🔘 Botão clicado:", action);
+        this.onInvokeActivity = async (context) => {
+            if (context.activity.name === 'adaptiveCard/action') {
+                const actionData = context.activity.value?.action?.data;
+                if (actionData?.actions === "Informo") {
+                    await context.sendActivity("✅ Tarefa marcada como finalizada!");
+                }
 
-            if (action.verb === "avancar") {
-                await context.sendActivity("✅ Você clicou em avançar!");
-
-                const profile = await this.userProfileAccessor.get(context, {} as userStateInfo);
-                // profile.responded = true;
-                await this.userProfileAccessor.set(context, profile);
+                return { status: 200 };
             }
-        }
 
-        return await super.onInvokeActivity(context);
+            return { status: 200 };
+        };
     }
 
     async run(context: TurnContext): Promise<void> {
         await super.run(context);
 
-        await this.ConversationState.saveChanges(context, false)
-        await this.UserState.saveChanges(context, false)
+        await this.ConversationState.saveChanges(context, false);
+        await this.UserState.saveChanges(context, false);
     }
 }
